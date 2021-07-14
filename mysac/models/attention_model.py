43a75a8ddd65345e2b52434ec582f42c4fc397b3
Samuel <samuel.chenatti@gmail.com>
2021-07-13 21:04:55 -0300

    Adds pre-lstm attention model

--- mysac/models/attention_model.py
+++ mysac/models/attention_model.py
@@ -16,24 +16,54 @@ W_INIT_VALUE = 3e-3
 
 class AttentionBase(nn.Module):
     def __init__(
-        self, num_inputs: int, num_actions: int, hidden_size: int = 256
+        self, num_inputs: int, num_actions: int, hidden_size: int = 256,
+        post_attention: bool = True
     ):
         super(AttentionBase, self).__init__()
+        print('Attention model; post_attention:', post_attention)
+
+        self.post_attention = post_attention
 
         self.recurrent_layer = nn.LSTM(
             num_inputs, hidden_size, batch_first=True)
 
-        self.attention = nn.Linear(hidden_size, hidden_size)
+        if self.post_attention:
+            self.attention = nn.Sequential(
+                nn.Linear(hidden_size, hidden_size),
+                nn.Softmax(dim=1)
+            )
 
-        self.softmax = torch.nn.Softmax(dim=1)
+        else:
+            self.attention = nn.Sequential(
+                nn.Linear(num_inputs, num_inputs),
+                nn.Softmax(dim=1)
+            )
 
-    def forward(self, state: torch.tensor):
+            self.dense = nn.Sequential(
+                nn.Linear(num_inputs, num_inputs),
+                nn.ReLU(),
+                nn.Linear(num_inputs, num_inputs),
+            )
+
+    def _forward_lstm(self, state: torch.tensor) -> torch.tensor:
         _, (state, _) = self.recurrent_layer(state)
-        state = state.squeeze(0)
 
-        attention_mask = self.softmax(self.attention(state))
+        return state.squeeze(0)
 
-        return state * attention_mask
+    def forward(self, state: torch.tensor) -> torch.tensor:
+        if self.post_attention:
+            state = self._forward_lstm(state)
+            attention_mask = self.attention(state)
+
+            state = state * attention_mask
+
+        else:
+            attention_mask = self.attention(state)
+            state = self.dense(state)
+
+            state = self._forward_lstm(state)
+
+        return state
 
 
 class QModel(AttentionBase):
@@ -57,13 +87,15 @@ class PolicyModel(AttentionBase):
             self,
             num_inputs: int,
             num_actions: int,
-            hidden_size: int = 256
+            hidden_size: int = 256,
+            **kwargs
     ):
 
         super().__init__(
             num_inputs=num_inputs,
             num_actions=num_actions,
-            hidden_size=hidden_size
+            hidden_size=hidden_size,
+            **kwargs
         )
 
         self.mean_linear = nn.Linear(hidden_size, num_actions)
