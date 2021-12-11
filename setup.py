221afdd4e635baa5b72e26af33ff057ac20eb45c
Samuel <samuel.chenatti@gmail.com>
2021-12-11 18:46:21 -0300

    Adds Dockerfile and fixes setup

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@ import setuptools
 
 print('Installing packages:', setuptools.find_packages())
 
+with open('requirements.txt') as reqs_file:
+    install_requires = [
+        line
+        for line in reqs_file
+    ]
+
 setuptools.setup(
     name="sac-experiments",
     version="0.0.1",
@@ -9,5 +15,6 @@ setuptools.setup(
     author_email="samuel.chenatti@gmail.com",
     description="SAC implementation for experimentation with new models",
     packages=setuptools.find_packages(),
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    install_requires=install_requires
 )
