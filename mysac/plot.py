e7c1841735a436d356c591b791561e4cb389d1d3
Samuel Felipe Chenatti <samuel.chenatti@gmail.com>
2023-08-21 21:51:29 -0300

    Adjust plot.py for plotting num steps

--- mysac/plot.py
+++ mysac/plot.py
@@ -2,12 +2,16 @@ import matplotlib.pyplot as plt
 import sys
 
 if __name__ == '__main__':
+    rewards = []
+    steps = []
+
     with open(sys.argv[1]) as stats_file:
-        rewards = [
-            float(line)
-            for line in stats_file
-        ]
+        for line in stats_file:
+            reward, step = line.split(';')
+
+            rewards.append(float(reward))
+            steps.append(float(step))
 
     plt.title(sys.argv[1])
-    plt.plot(rewards)
+    plt.plot(steps)
     plt.show()
